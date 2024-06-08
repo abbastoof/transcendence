809da636bca0c60012667ad20ff086ef3c88bbd8
sepehr toof <sepehr@sepehrs-iMac.local>
2024-06-08 16:16:01 +0300

    deleted token and refresh path from url on user-management urls on project level

--- Backend/user-management/user-management/user-management/urls.py
+++ Backend/user-management/user-management/user-management/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('', include('users.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
 ]
