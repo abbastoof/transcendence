3cd926ef5a9a691505168b1817c64716123df83b
Mojtaba Toof <mtoof@c1r3p3.hive.fi>
2024-06-11 12:34:00 +0300

    kafka docker is done

--- Backend/kafka/start_kafka.sh
+++ Backend/kafka/start_kafka.sh
@@ -1,4 +1,10 @@
-#! /bin/bash
+#!/bin/bash
+
+# Start Zookeeper in the foreground and redirect logs
 bin/zookeeper-server-start.sh config/zookeeper.properties > zookeeper.log 2>&1 &
-sleep 5
+
+# Wait for Zookeeper to start
+sleep 10
+
+# Start Kafka in the foreground and redirect logs
 bin/kafka-server-start.sh config/server.properties > kafka.log 2>&1 &
