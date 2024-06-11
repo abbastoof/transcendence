4dad0e63e02dce252a19000970e45934fe0131b2
mtoof <mojtaba.toof@gmail.com>
2024-06-11 12:55:37 +0300

    added CorsMiddleware to settings.py files on projects level

--- Backend/user_management/user_management/user_management/settings.py
+++ Backend/user_management/user_management/user_management/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'user_management.urls'
