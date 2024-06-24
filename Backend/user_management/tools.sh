eed7b8fdb9d236fec7f27c0a8060c108231e9e6d
Abbas Toof <abbas.toof@gmail.com>
2024-06-24 22:16:16 +0300

    trying to route the backend api endpoints in nginx reverse proxy in Frontend nginx.conf

--- Backend/user_management/tools.sh
+++ Backend/user_management/tools.sh
@@ -14,4 +14,4 @@ export DJANGO_SETTINGS_MODULE=user_management.settings
 python3 /app/user_management/manage.py makemigrations users
 python3 /app/user_management/manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('${DB_USER}', 'admin@example.com', '${DB_USER}')" | python3 /app/user_management/manage.py shell && echo "Superuser created successfully."
-python3 /app/user_management/manage.py runserver 0.0.0.0:8000
+python3 /app/user_management/manage.py runserver 0.0.0.0:8001
