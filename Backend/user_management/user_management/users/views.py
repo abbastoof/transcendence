98f293546c203e1a9ac03d7bbbc4667b4c30c078
sepehr toof <sepehr@sepehrs-iMac.local>
2024-06-29 14:21:01 +0300

    refactored views.py in auth-service, no need to save JWT and user in a database, added new feature to it to generate token using refresh token

--- Backend/user_management/user_management/users/views.py
+++ Backend/user_management/user_management/users/views.py
@@ -15,22 +15,76 @@ from .serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ViewSet):
+    """
+        UserViewSet class to handle user related operations.
+
+        This class inherits from ViewSet. It defines the methods to handle user related operations like
+        getting the list of users, retrieving a user, updating a user, and deleting a user.
+
+        Attributes:
+            authentication_classes: The list of authentication classes to use for the view.
+            permission_classes: The list of permission classes to use for the view.
+        
+        Methods:
+            users_list: Method to get the list of users.
+            retrieve_user: Method to retrieve a user.
+            update_user: Method to update a user.
+            destroy_user: Method to delete a user.
+            handle_rabbitmq_request: Method to handle the RabbitMQ request.
+            start_consumer: Method to start the RabbitMQ consumer.
+    
+    """
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def users_list(self, request):
+    def users_list(self, request) -> Response:
+        """
+            Method to get the list of users.
+            
+            This method gets the list of users from the database and returns the list of users.
+            
+            Args:
+                request: The request object.
+
+            Returns:
+                Response: The response object containing the list of users.
+        """
         if not request.user.is_staff or not request.user.is_superuser:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
 
-    def retrieve_user(self, request, pk=None):
+    def retrieve_user(self, request, pk=None) -> Response:
+        """
+            Method to retrieve a user.
+
+            This method retrieves a user from the database using the user id and returns the user data.
+
+            Args:
+                request: The request object.
+                pk: The primary key of the user.
+            
+            Returns:
+                Response: The response object containing the user data.
+        """
         data = get_object_or_404(User, id=pk)
         serializer = UserSerializer(data)
         return Response(serializer.data)
 
-    def update_user(self, request, pk=None):
+    def update_user(self, request, pk=None) -> Response:
+        """
+            Method to update a user.
+            
+            This method updates a user in the database using the user id and the data in the request.
+            
+            Args:
+                request: The request object containing the user data.
+                pk: The primary key of the user.
+                
+            Returns:
+                Response: The response object containing the updated user data.
+        """
         data = get_object_or_404(User, id=pk)
         if data != request.user and not request.user.is_superuser:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
@@ -40,7 +94,19 @@ class UserViewSet(viewsets.ViewSet):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
-    def destroy_user(self, request, pk=None):
+    def destroy_user(self, request, pk=None) -> Response:
+        """
+            Method to delete a user.
+            
+            This method deletes a user from the database using the user id.
+            
+            Args:
+                request: The request object.
+                pk: The primary key of the user.
+                
+            Returns:
+                Response: The response object containing the status of the deletion.
+        """
         data = get_object_or_404(User, id=pk)
         if data != request.user and not request.user.is_superuser:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
@@ -52,7 +118,22 @@ class UserViewSet(viewsets.ViewSet):
     @method_decorator(
         csrf_exempt
     )  # This decorator is used to disable CSRF protection for this method because it is called by RabbitMQ and not by a browser
-    def handle_rabbitmq_request(ch, method, properties, body):
+    def handle_rabbitmq_request(ch, method, properties, body) -> None:
+        """
+            Method to handle the RabbitMQ request.
+            
+            This method handles the RabbitMQ request by authenticating the user and sending the response message.
+            
+            Args:
+                ch: The channel object.
+                method: The method object.
+                properties: The properties object.
+                body: The body of the message.
+                
+            Returns:
+                None
+            
+        """
         payload = json.loads(body)
         username = payload.get("username")
         password = payload.get("password")
@@ -72,14 +153,36 @@ class UserViewSet(viewsets.ViewSet):
         print(f"Response message: {response_message}")
         publish_message("auth_response_queue", json.dumps(response_message))
 
-    def start_consumer(self):
+    def start_consumer(self) -> None:
         consume_message("user_request_queue", self.handle_rabbitmq_request)
 
 
 class RegisterViewSet(viewsets.ViewSet):
+    """
+        RegisterViewSet class to handle user registration.
+        
+        This class inherits from ViewSet. It defines the method to handle user registration.
+
+        Attributes:
+            permission_classes: The list of permission classes to use for the view.
+
+        Methods:
+            create_user: Method to create a new user.
+    """
     permission_classes = [AllowAny]
 
-    def create_user(self, request):
+    def create_user(self, request) -> Response:
+        """
+            Method to create a new user.
+
+            This method creates a new user by validating the data in the request and saving the user data to the database.
+
+            Args:
+                request: The request object containing the user data.
+            
+            Returns:
+                Response: The response object containing the user data.
+        """
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
