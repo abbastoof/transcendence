cb86fda8497b26177c4b7bd5ce9d8435800ca60b
Abbas Toof <abbas.toof@gmail.com>
2024-07-08 23:18:45 +0300

    fix: Correct return type in CustomTokenObtainPairSerializer.get_token method

--- Backend/auth_service/auth_service/user_auth/serializers.py
+++ Backend/auth_service/auth_service/user_auth/serializers.py
@@ -1,22 +1,24 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-import asyncio
+from rest_framework_simplejwt.tokens import Token
+
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
-    def get_token(cls, user) -> dict:
+    def get_token(cls, user) -> Token:
 
         """
             Get token method to generate tokens for the user.
 
-            This method overrides the get_token method of TokenObtainPairSerializer to generate tokens for the user. 
+            This method overrides the get_token method of TokenObtainPairSerializer to generate tokens for the user.
             It generates the tokens for the user and returns the tokens.
 
             Args:
                 user: The user object.
-            
+
             Returns:
                 dict: The dictionary containing the tokens.
         """
         token = super().get_token(user)
         token["custom_claims"] = {"username": user.username, "password": user.password}
         return token
+
