6e751cd895f8fbd30c10398b7157243ac8d1b680
Abbas Toof <abbas.toof@gmail.com>
2024-08-05 16:01:00 +0300

    added username to game-history model,updated serializer and test file

--- Backend/game_history/game_history/game_data/serializers.py
+++ Backend/game_history/game_history/game_data/serializers.py
@@ -4,9 +4,12 @@ from rest_framework import serializers
 from .models import GameHistory, GameStat
 
 class GameHistorySerializer(serializers.ModelSerializer):
+    game_summary = serializers.SerializerMethodField()
     class Meta:
         model = GameHistory
         fields = '__all__'
+    def get_game_summary(self, obj):
+        return str(obj)
 class GameStatSerializer(serializers.ModelSerializer):
     class Meta:
         model = GameStat
