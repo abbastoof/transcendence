3778ce25973b83367ab8f14279c846addde69d81
Nick <nicktvdd@hotmail.com>
2024-08-08 15:26:31 +0300

    fixed profile change buttons

--- Frontend/src/js/modals/profile.js
+++ Frontend/src/js/modals/profile.js
@@ -21,20 +21,20 @@ export function updateUserProfile() {
             'Authorization': `Bearer ${userData.token}`
         }
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Fetched Data:', data); // Debugging line
-        const userProfileContainer = document.getElementById('userProfile');
-        if (!userProfileContainer) {
-            console.error('UserProfile container not found');
-            return;
-        }
-        const htmlContent = `
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Fetched Data:', data); // Debugging line
+            const userProfileContainer = document.getElementById('userProfile');
+            if (!userProfileContainer) {
+                console.error('UserProfile container not found');
+                return;
+            }
+            const htmlContent = `
             <div class="container mt-4">
                 <div class="profile-header d-flex align-items-center">
                     <img id="avatar" src="${data.avatar || '/media/default.jpg'}" alt="User Profile Image" class="img-thumbnail mb-4 mr-3" width="80" height="80">
@@ -73,6 +73,25 @@ export function updateUserProfile() {
 
             updateFriendsList();
             updateMatchHistory();
+
+            // Toggle email update form visibility
+            document.getElementById('changeEmailButton').addEventListener('click', () => {
+                document.getElementById('updateEmailForm').style.display =
+                    document.getElementById('updateEmailForm').style.display === 'none' ? 'block' : 'none';
+            });
+
+            // Toggle password update form visibility
+            document.getElementById('changePasswordButton').addEventListener('click', () => {
+                document.getElementById('updatePasswordForm').style.display =
+                    document.getElementById('updatePasswordForm').style.display === 'none' ? 'block' : 'none';
+            });
+
+            // Toggle profile picture update form visibility
+            document.getElementById('changeProfilePictureButton').addEventListener('click', () => {
+                document.getElementById('imageUploadForm').style.display =
+                    document.getElementById('imageUploadForm').style.display === 'none' ? 'block' : 'none';
+            });
+
             const form = document.getElementById('imageUploadForm');
             form.addEventListener('submit', (event) => {
                 event.preventDefault();
@@ -113,130 +132,111 @@ export function updateUserProfile() {
         })
         .catch(error => {
             console.error('Error fetching user data:', error);
-        ;
-        userProfileContainer.innerHTML = htmlContent;
 
-        // Toggle email update form visibility
-        document.getElementById('changeEmailButton').addEventListener('click', () => {
-            document.getElementById('updateEmailForm').style.display = 
-                document.getElementById('updateEmailForm').style.display === 'none' ? 'block' : 'none';
-        });
 
-        // Handle email update
-        document.getElementById('updateEmailForm').addEventListener('submit', (event) => {
-            event.preventDefault();
-            const newEmail = document.getElementById('newEmail').value;
-            if (!newEmail) {
-                console.error('Email cannot be empty');
-                return;
-            }
-
-            fetch(`/user/${userData.id}/`, {
-                method: 'PUT',
-                headers: {
-                    'Authorization': `Bearer ${userData.token}`,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ email: newEmail })
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
+            // Handle email update
+            document.getElementById('updateEmailForm').addEventListener('submit', (event) => {
+                event.preventDefault();
+                const newEmail = document.getElementById('newEmail').value;
+                if (!newEmail) {
+                    console.error('Email cannot be empty');
+                    return;
                 }
-                return response.json();
-            })
-            .then(data => {
-                console.log('Email updated successfully:', data);
-                document.getElementById('emailText').innerText = data.email;
-                document.getElementById('updateEmailForm').style.display = 'none';
-            })
-            .catch(error => {
-                console.error('Error updating email:', error);
+
+                fetch(`/user/${userData.id}/`, {
+                    method: 'PUT',
+                    headers: {
+                        'Authorization': `Bearer ${userData.token}`,
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ email: newEmail })
+                })
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        console.log('Email updated successfully:', data);
+                        document.getElementById('emailText').innerText = data.email;
+                        document.getElementById('updateEmailForm').style.display = 'none';
+                    })
+                    .catch(error => {
+                        console.error('Error updating email:', error);
+                    });
             });
-        });
 
-        // Toggle password update form visibility
-        document.getElementById('changePasswordButton').addEventListener('click', () => {
-            document.getElementById('updatePasswordForm').style.display = 
-                document.getElementById('updatePasswordForm').style.display === 'none' ? 'block' : 'none';
-        });
-
-        // Handle password update
-        document.getElementById('updatePasswordForm').addEventListener('submit', (event) => {
-            event.preventDefault();
-            const newPassword = document.getElementById('newPassword').value;
-            if (!newPassword) {
-                console.error('Password cannot be empty');
-                return;
-            }
-
-            fetch(`/user/${userData.id}/`, {
-                method: 'PUT',
-                headers: {
-                    'Authorization': `Bearer ${userData.token}`,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ password: newPassword })
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
+            // Handle password update
+            document.getElementById('updatePasswordForm').addEventListener('submit', (event) => {
+                event.preventDefault();
+                const newPassword = document.getElementById('newPassword').value;
+                if (!newPassword) {
+                    console.error('Password cannot be empty');
+                    return;
                 }
-                return response.json();
-            })
-            .then(data => {
-                console.log('Password updated successfully:', data);
-                alert('Password updated successfully');
-                document.getElementById('updatePasswordForm').style.display = 'none';
-            })
-            .catch(error => {
-                console.error('Error updating password:', error);
+
+                fetch(`/user/${userData.id}/`, {
+                    method: 'PUT',
+                    headers: {
+                        'Authorization': `Bearer ${userData.token}`,
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ password: newPassword })
+                })
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        console.log('Password updated successfully:', data);
+                        alert('Password updated successfully');
+                        document.getElementById('updatePasswordForm').style.display = 'none';
+                    })
+                    .catch(error => {
+                        console.error('Error updating password:', error);
+                    });
             });
-        });
 
-        // Toggle profile picture update form visibility
-        document.getElementById('changeProfilePictureButton').addEventListener('click', () => {
-            document.getElementById('imageUploadForm').style.display = 
-                document.getElementById('imageUploadForm').style.display === 'none' ? 'block' : 'none';
-        });
-
-        // Handle image upload
-        document.getElementById('imageUploadForm').addEventListener('submit', (event) => {
-            event.preventDefault();
-            const imageInput = document.getElementById('imageInput');
-            const file = imageInput.files[0];
-            if (!file) {
-                console.error('No image selected');
-                return;
-            }
-            const formData = new FormData();
-            formData.append('avatar', file);
-
-            fetch(`/user/${userData.id}/`, {
-                method: 'PUT',
-                headers: {
-                    'Authorization': `Bearer ${userData.token}`
-                },
-                body: formData
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
+            // Handle image upload
+            document.getElementById('imageUploadForm').addEventListener('submit', (event) => {
+                event.preventDefault();
+                const imageInput = document.getElementById('imageInput');
+                const file = imageInput.files[0];
+                if (!file) {
+                    console.error('No image selected');
+                    return;
                 }
-                return response.json();
-            })
-            .then(data => {
-                console.log('Image uploaded successfully:', data);
-                document.getElementById('avatar').src = `${data.avatar}?t=${new Date().getTime()}`;
-                document.getElementById('imageUploadForm').style.display = 'none';
-            })
-            .catch(error => {
-                console.error('Error uploading image:', error);
-            });
-        });
+                const formData = new FormData();
+                formData.append('avatar', file);
 
-    })
-    .catch(error => {
-        console.error('Error fetching user data:', error);
-    });
+                fetch(`/user/${userData.id}/`, {
+                    method: 'PUT',
+                    headers: {
+                        'Authorization': `Bearer ${userData.token}`
+                    },
+                    body: formData
+                })
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        console.log('Image uploaded successfully:', data);
+                        document.getElementById('avatar').src = `${data.avatar}?t=${new Date().getTime()}`;
+                        document.getElementById('imageUploadForm').style.display = 'none';
+                    })
+                    .catch(error => {
+                        console.error('Error uploading image:', error);
+                    });
+            });
+
+        })
+        .catch(error => {
+            console.error('Error fetching user data:', error);
+        });
 }
