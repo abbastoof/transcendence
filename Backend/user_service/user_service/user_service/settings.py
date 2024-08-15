05cc2505a0849d26a7bb4669768f3fa391aa1a00
mtoof <mojtaba.toof@gmail.com>
2024-08-15 15:39:44 +0300

    added otp_status, otp, otp_expiry_time to models.py in user_service, added the same fields to serializer, updated login view function to send email to otp enabled users, added verify_otp to views.py and updated urls.py and added email settings to setting.py

--- Backend/user_service/user_service/user_service/settings.py
+++ Backend/user_service/user_service/user_service/settings.py
@@ -13,7 +13,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
 import re
+
+load_dotenv()
 
 LOG_DIR = Path('/var/log/')
 
@@ -80,6 +83,13 @@ DEBUG = True
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.Argon2PasswordHasher",
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('USER_SERVICE_EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('USER_SERVICE_PASS')
 
 ALLOWED_HOSTS = [
     'localhost',
